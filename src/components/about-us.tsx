eaddb6f7ff3f6c2c3c65100d4fd2a6e07c551fc6
variantxx <juanitobaldoaspler@gmail.com>
2025-07-25 18:31:36 +0800

    home page updated

--- src/components/about-us.tsx
+++ src/components/about-us.tsx
@@ -8,6 +8,38 @@ import {
 import ImgBaldo from "../assets/images/img_baldo.jpg";
 import ImgFerrer from "../assets/images/img_ferrer.jpg";
 import ImgAlvarez from "../assets/images/img_alvarez.jpg";
+import ImgArmada from "../assets/images/img_armada.jpg";
+
+const teamMembers = [
+  {
+    name: "Rolando Ferrer",
+    role: "Web Security / Back-end Developer",
+    image: ImgFerrer,
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus ornare rutrum sem, eget maximus mauris lobortis a.",
+  },
+  {
+    name: "Juanito Baldo Jr.",
+    role: "Full-stack Developer",
+    image: ImgBaldo,
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus ornare rutrum sem, eget maximus mauris lobortis a.",
+  },
+  {
+    name: "Joshua Alvarez",
+    role: "UI/UX Designer",
+    image: ImgAlvarez,
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus ornare rutrum sem, eget maximus mauris lobortis a.",
+  },
+  {
+    name: "Joyce Armada",
+    role: "Coach",
+    image: ImgArmada,
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus ornare rutrum sem, eget maximus mauris lobortis a.",
+  },
+];
 
 export const AboutUs = () => {
   return (
@@ -20,75 +52,37 @@ export const AboutUs = () => {
         Meet the team behind the Sibol Homes
       </p>
 
-      <div className="mt-14 grid grid-cols-3 gap-8">
-        <div className="p-8 w-sm flex flex-col items-center justify-center bg-white rounded-2xl shadow-medium shadow-gray-300">
-          <Avatar className="w-36 h-36 text-large" size="lg" src={ImgFerrer} />
-          <span className="mt-6 text-xl font-bold text-foreground-700">
-            Rolando Ferrer.
-          </span>
-          <span className="text-primary">
-            Web Security / Back-end Developer
-          </span>
-          <p className="text-center mt-4 text-foreground-700">
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus
-            ornare rutrum sem, eget maximus mauris lobortis a.
-          </p>
-          <div className="mt-4">
-            <Button isIconOnly radius="full" variant="light">
-              <IconBrandLinkedin />
-            </Button>
-            <Button isIconOnly radius="full" variant="light">
-              <IconBrandFacebook />
-            </Button>
-            <Button isIconOnly radius="full" variant="light">
-              <IconBrandGithub />
-            </Button>
+      <div className="mt-14 grid grid-cols-4 gap-8">
+        {teamMembers.map((member, index) => (
+          <div
+            key={index}
+            className="p-8 flex flex-col items-center justify-center bg-white rounded-2xl shadow-medium shadow-gray-300"
+          >
+            <Avatar
+              className="w-36 h-36 text-large"
+              size="lg"
+              src={member.image}
+            />
+            <span className="mt-6 text-xl font-bold text-foreground-700">
+              {member.name}
+            </span>
+            <span className="text-primary">{member.role}</span>
+            <p className="text-center mt-4 text-foreground-700">
+              {member.description}
+            </p>
+            <div className="mt-4">
+              <Button isIconOnly radius="full" variant="light">
+                <IconBrandLinkedin />
+              </Button>
+              <Button isIconOnly radius="full" variant="light">
+                <IconBrandFacebook />
+              </Button>
+              <Button isIconOnly radius="full" variant="light">
+                <IconBrandGithub />
+              </Button>
+            </div>
           </div>
-        </div>
-        <div className="p-8 w-sm flex flex-col items-center justify-center bg-white rounded-2xl shadow-medium shadow-gray-300">
-          <Avatar className="w-36 h-36 text-large" size="lg" src={ImgBaldo} />
-          <span className="mt-6 text-xl font-bold text-foreground-700">
-            Juanito Baldo Jr.
-          </span>
-          <span className="text-primary">Front-end Developer</span>
-          <p className="text-center mt-4 text-foreground-700">
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus
-            ornare rutrum sem, eget maximus mauris lobortis a.
-          </p>
-          <div className="mt-4">
-            <Button isIconOnly radius="full" variant="light">
-              <IconBrandLinkedin />
-            </Button>
-            <Button isIconOnly radius="full" variant="light">
-              <IconBrandFacebook />
-            </Button>
-            <Button isIconOnly radius="full" variant="light">
-              <IconBrandGithub />
-            </Button>
-          </div>
-        </div>
-        <div className="p-8 w-sm flex flex-col items-center justify-center bg-white rounded-2xl shadow-medium shadow-gray-300">
-          <Avatar className="w-36 h-36 text-large" size="lg" src={ImgAlvarez} />
-          <span className="mt-6 text-xl font-bold text-foreground-700">
-            Joshua Alvarez
-          </span>
-          <span className="text-primary">UI/UX Designer</span>
-          <p className="text-center mt-4 text-foreground-700">
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus
-            ornare rutrum sem, eget maximus mauris lobortis a.
-          </p>
-          <div className="mt-4">
-            <Button isIconOnly radius="full" variant="light">
-              <IconBrandLinkedin />
-            </Button>
-            <Button isIconOnly radius="full" variant="light">
-              <IconBrandFacebook />
-            </Button>
-            <Button isIconOnly radius="full" variant="light">
-              <IconBrandGithub />
-            </Button>
-          </div>
-        </div>
+        ))}
       </div>
     </div>
   );
--- src/components/hero.tsx
+++ src/components/hero.tsx
@@ -1,28 +1,36 @@
 import { Button, Input } from "@heroui/react";
-import { IconSearch } from "@tabler/icons-react";
+
+import ImgHeroBackground from "../assets/images/img_hero_background.jpg";
 
 export const Hero = () => {
   return (
-    <div className="h-screen flex flex-col items-center justify-center">
-      <h1 className="text-6xl text-center font-bold">
-        Find Your Perfect
-        <br />
-        Dream Home
-      </h1>
-      <p className="text-2xl text-center mt-8 max-w-1/2">
-        Discover exceptional properties in prime locations with our AI powered
-        search and expert guidance
-      </p>
+    <div className="h-screen flex flex-col items-center justify-center relative">
+      <img
+        alt="Hero Background"
+        className="absolute inset-0 w-full h-full object-cover z-0"
+        src={ImgHeroBackground}
+      />
+      <div className="h-full w-full absolute z-0 bg-gradient-to-b from-white to-transparent" />
+
+      <div className="z-10 flex flex-col absolute top-20 left-20">
+        <h1 className="text-6xl font-bold text-foreground-700">Dream Home</h1>
+        <p className="text-2xl mt-8 max-w-1/2 text-foreground-700">
+          Discover exceptional properties in prime locations with our AI powered
+          search and expert guidance
+        </p>
+      </div>
 
       {/* Search input */}
       <div
-        className="max-w-lg mx-auto mt-20 bg-white rounded-2xl shadow-large shadow-gray-300 p-6
+        className="w-lg mx-auto mt-26 bg-white rounded-2xl shadow-large shadow-gray-300 p-6
         flex
-        flex-col"
+        flex-col
+        z-10"
       >
         <div className="flex items-center gap-4">
           <Input
             className="w-full"
+            color="primary"
             placeholder="Find a property..."
             size="lg"
             type="text"
