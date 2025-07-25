4f1fd30c72e635fb23a0d1d4048d8514dffb5c96
variantxx <juanitobaldoaspler@gmail.com>
2025-07-25 13:39:59 +0800

    footer updated

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,4 +1,4 @@
-import { Button } from "@heroui/react";
+import { Button, Link } from "@heroui/react";
 import {
   IconBrandInstagram,
   IconBrandFacebook,
@@ -9,7 +9,34 @@ export const Footer = () => {
   return (
     <footer className="bg-gray-500 text-white p-8 ">
       <div className="container mx-auto flex flex-col">
-        <div className="flex items-center justify-between">
+        <div className="grid grid-cols-3 gap-8">
+          <div>
+            <h1 className="text-2xl font-bold">SIBOL</h1>
+            <ul className="mt-4 flex items-start gap-8">
+              <li>
+                <Link className="text-white" href="/home" underline="always">
+                  Home
+                </Link>
+              </li>
+              <li>
+                <Link
+                  className="text-white"
+                  href="/properties"
+                  underline="always"
+                >
+                  Properties
+                </Link>
+              </li>
+              <li>
+                <Link className="text-white" href="/account" underline="always">
+                  Account
+                </Link>
+              </li>
+            </ul>
+          </div>
+        </div>
+
+        <div className="mt-8 flex items-center justify-between">
           <p>
             Copyright &copy; {new Date().getFullYear()} Sibol. All rights
             reserved.
