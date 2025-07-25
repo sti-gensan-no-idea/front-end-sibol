44b1f3102cb857d1f83dd0f871c082519ee9ce40
variantxx <juanitobaldoaspler@gmail.com>
2025-07-25 17:18:29 +0800

    navbar updated

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,121 +1,39 @@
-import React from "react";
-import {
-  IconHome,
-  IconBuildingCommunity,
-  IconUserSquareRounded,
-} from "@tabler/icons-react";
-import {
-  Navbar,
-  NavbarBrand,
-  NavbarContent,
-  NavbarItem,
-  NavbarMenuToggle,
-  NavbarMenu,
-  NavbarMenuItem,
-  Link,
-  Button,
-  Tab,
-  Tabs,
-} from "@heroui/react";
+import { Button, Navbar, Tab, Tabs } from "@heroui/react";
+import { IconArrowNarrowRight } from "@tabler/icons-react";
 
 export const NavBar = () => {
-  const [isMenuOpen, setIsMenuOpen] = React.useState(false);
-
-  const menuItems = [
-    "Profile",
-    "Dashboard",
-    "Activity",
-    "Analytics",
-    "System",
-    "Deployments",
-    "My Settings",
-    "Team Settings",
-    "Help & Feedback",
-    "Log Out",
-  ];
-
   return (
     <Navbar
-      className="border-t-4 border-solid border-t-primary"
+      className="border-t-4 border-solid border-t-primary h-26"
       maxWidth="full"
-      onMenuOpenChange={setIsMenuOpen}
     >
-      <NavbarContent>
-        <NavbarMenuToggle
-          aria-label={isMenuOpen ? "Close menu" : "Open menu"}
-          className="sm:hidden"
-        />
-        <NavbarBrand>
-          <h1 className="font-bold text-xl">SIBOL</h1>
-        </NavbarBrand>
-      </NavbarContent>
-
-      <NavbarContent className="hidden sm:flex gap-4" justify="center">
-        <div className="flex flex-wrap gap-4">
-          <Tabs aria-label="Tabs variants" color="primary" variant="underlined">
-            <Tab
-              key="home"
-              title={
-                <div className="flex items-center space-x-2">
-                  <IconHome />
-                  <span>Home</span>
-                </div>
-              }
-            />
-            <Tab
-              key="properties"
-              title={
-                <div className="flex items-center space-x-2">
-                  <IconBuildingCommunity />
-                  <span>Properties</span>
-                </div>
-              }
-            />
-            <Tab
-              key="account"
-              title={
-                <div className="flex items-center space-x-2">
-                  <IconUserSquareRounded />
-                  <span>Account</span>
-                </div>
-              }
-            />
-          </Tabs>
-        </div>
-      </NavbarContent>
-      <NavbarContent justify="end">
-        <NavbarItem>
-          <Button
-            as={Link}
-            color="primary"
-            href="#"
-            radius="full"
-            variant="solid"
-          >
-            Sign In
-          </Button>
-        </NavbarItem>
-      </NavbarContent>
-      <NavbarMenu>
-        {menuItems.map((item, index) => (
-          <NavbarMenuItem key={`${item}-${index}`}>
-            <Link
-              className="w-full"
-              color={
-                index === 2
-                  ? "primary"
-                  : index === menuItems.length - 1
-                    ? "danger"
-                    : "foreground"
-              }
-              href="#"
+      <div className="flex flex-col justify-between">
+        <div>
+          <h1 className="text-2xl ml-4 font-bold">Sibol Homes</h1>
+          <div className="flex flex-wrap gap-4">
+            <Tabs
+              aria-label="Tabs variants"
+              color="primary"
               size="lg"
+              variant="underlined"
             >
-              {item}
-            </Link>
-          </NavbarMenuItem>
-        ))}
-      </NavbarMenu>
+              <Tab key="home" title="Home" />
+              <Tab key="properties" title="Properties" />
+              <Tab key="contact" title="Contact" />
+              <Tab key="about" title="About Us" />
+            </Tabs>
+          </div>
+        </div>
+      </div>
+      <div className="flex items-center justify-center">
+        <Button className="mr-3" color="primary" radius="full" variant="ghost">
+          Sign In
+        </Button>
+        <Button color="primary" radius="full" variant="shadow">
+          Get Started
+          <IconArrowNarrowRight />
+        </Button>
+      </div>
     </Navbar>
   );
 };
