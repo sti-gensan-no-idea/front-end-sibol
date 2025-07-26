29136facee192ca95b4b1cc4fad0fce3319ce50e
variantxx <juanitobaldoaspler@gmail.com>
2025-07-26 22:38:20 +0800

    blur background added

--- src/components/signup-card-form.tsx
+++ src/components/signup-card-form.tsx
@@ -3,11 +3,16 @@ import {
   IconMailFilled,
   IconUserFilled,
   IconLockFilled,
+  IconMapPinFilled,
 } from "@tabler/icons-react";
 
 export const SignUpCardForm = () => {
   return (
-    <div className="container mx-auto p-8 h-screen flex flex-col items-center justify-center">
+    <div className="container mx-auto p-8 h-screen flex flex-col items-center justify-center relative">
+      <div className="absolute top-46 -left-10 w-72 h-72 bg-green-300 rounded-full blur-3xl opacity-30" />
+      <div className="absolute top-20 right-20 w-60 h-60 bg-primary-300 rounded-full blur-2xl opacity-20" />
+      <div className="absolute bottom-20 left-1/3 w-96 h-96 bg-red-300 rounded-full blur-[100px] opacity-20" />
+
       <div
         className="w-lg mx-auto bg-white rounded-2xl shadow-large shadow-gray-300 p-8
         flex
@@ -22,34 +27,42 @@ export const SignUpCardForm = () => {
         <span className="flex items-center justify-center text-foreground-700 mt-3">
           Join thousands of properties enthusiasts
         </span>
-        <Input
-          className="mt-8"
-          label="Full name"
-          placeholder="Full name"
-          startContent={<IconUserFilled />}
-          type="text"
-        />
-        <Input
-          className="mt-4"
-          label="Email"
-          placeholder="Email address"
-          startContent={<IconMailFilled />}
-          type="email"
-        />
-        <Input
-          className="mt-4"
-          label="Set password"
-          placeholder="Set password"
-          startContent={<IconLockFilled />}
-          type="password"
-        />
-        <Input
-          className="mt-4"
-          label="Confirm password"
-          placeholder="Confirm password"
-          startContent={<IconLockFilled />}
-          type="password"
-        />
+        <div className="flex flex-col w-full mt-8">
+          <Input
+            label="Full name"
+            placeholder="Full name"
+            startContent={<IconUserFilled />}
+            type="text"
+          />
+          <Input
+            className="mt-4"
+            label="Email"
+            placeholder="Email address"
+            startContent={<IconMailFilled />}
+            type="email"
+          />
+          <Input
+            className="mt-4"
+            label="Home address"
+            placeholder="Complete home address"
+            startContent={<IconMapPinFilled />}
+            type="text"
+          />
+          <Input
+            className="mt-4"
+            label="Set password"
+            placeholder="Set password"
+            startContent={<IconLockFilled />}
+            type="password"
+          />
+          <Input
+            className="mt-4"
+            label="Confirm password"
+            placeholder="Confirm password"
+            startContent={<IconLockFilled />}
+            type="password"
+          />
+        </div>
         <Button className="mt-8 w-full" color="primary">
           Submit
         </Button>
