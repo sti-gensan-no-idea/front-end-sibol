cca57a4504718a679eacbdea2a5992dd22589000
variantxx <juanitobaldoaspler@gmail.com>
2025-07-26 23:25:57 +0800

    404 page  updated

--- src/pages/page-not-found.tsx
+++ src/pages/page-not-found.tsx
@@ -3,6 +3,8 @@ import { useNavigate } from "react-router-dom";
 import { Button } from "@heroui/button";
 import { IconArrowNarrowRight } from "@tabler/icons-react";
 
+import { NavBar } from "@/components/navbar";
+
 export const NotFoundPage = () => {
   const navigate = useNavigate();
 
@@ -11,26 +13,29 @@ export const NotFoundPage = () => {
   }, []);
 
   return (
-    <div className="flex items-center justify-center flex-col h-screen">
-      <h1 className="text-foreground-700 text-9xl font-bold text-center select-none">
-        404
-      </h1>
-      <span className="text-3xl flex items-center justify-center select-none">
-        Page Not Found
-      </span>
-      <p className="mt-4">
-        The page you&apos;re looking for doesn&apos;t exist or has been moved.
-      </p>
-      <Button
-        className="mt-16"
-        color="primary"
-        size="lg"
-        variant="shadow"
-        onPress={() => navigate("/")}
-      >
-        Go to Homepage
-        <IconArrowNarrowRight />
-      </Button>
-    </div>
+    <>
+      <NavBar />
+      <div className="flex items-center justify-center flex-col h-screen w-full absolute top-0 -z-10">
+        <h1 className="text-foreground-700 text-9xl font-bold text-center select-none">
+          404
+        </h1>
+        <span className="text-3xl flex items-center justify-center select-none">
+          Page Not Found
+        </span>
+        <p className="mt-4">
+          The page you&apos;re looking for doesn&apos;t exist or has been moved.
+        </p>
+        <Button
+          className="mt-16"
+          color="primary"
+          size="lg"
+          variant="shadow"
+          onPress={() => navigate("/")}
+        >
+          Go to Homepage
+          <IconArrowNarrowRight />
+        </Button>
+      </div>
+    </>
   );
 };
