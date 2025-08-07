f229cfde43317b197ffd2447962f61fcae203abf
variantxx <juanitobaldoaspler@gmail.com>
2025-08-08 06:50:58 +0800

    agent welcome widget added

--- src/pages/agent/dashboard-agent.tsx
+++ src/pages/agent/dashboard-agent.tsx
@@ -1,13 +1,18 @@
 import { useEffect } from "react";
 
+import { AgentWelcome } from "@/widget/agent-welcome";
+
 export const DashboardAgentPage = () => {
   useEffect(() => {
     document.title = "Agent | Atuna";
   }, []);
 
   return (
-    <div>
-      <h1>Agent Page</h1>
+    <div className="flex container mx-auto">
+      <div className="w-30"></div>
+      <div className="flex-1 p-8">
+        <AgentWelcome />
+      </div>
     </div>
   );
 };
---
+++ src/widget/agent-welcome.tsx
@@ -0,0 +1,26 @@
+export const AgentWelcome = () => {
+  return (
+    <div className="rounded-large p-8 bg-white shadow-medium">
+      <h2 className="text-3xl font-bold">Good Morning, Agent Anna!</h2>
+      <span className="mt-2 flex">Heres your team performance overview</span>
+      <div className="grid grid-cols-4 gap-4 mt-8">
+        <div className="bg-gray-200 rounded-medium flex flex-col p-4">
+          <span className="flex font-bold text-2xl">18</span>
+          <span className="flex">Properties assigned</span>
+        </div>
+        <div className="bg-gray-200 rounded-medium flex flex-col p-4">
+          <span className="flex font-bold text-2xl">6</span>
+          <span className="flex">Active agents</span>
+        </div>
+        <div className="bg-gray-200 rounded-medium flex flex-col p-4">
+          <span className="flex font-bold text-2xl">₱5.3M</span>
+          <span className="flex">Total Sales (This month)</span>
+        </div>
+        <div className="bg-gray-200 rounded-medium flex flex-col p-4">
+          <span className="flex font-bold text-2xl">John Santos</span>
+          <span className="flex">Top Performer (8 Deals)</span>
+        </div>
+      </div>
+    </div>
+  );
+};
