13aa31b5756c73a3baa3cd7f4daccd7e4edf651b
variantxx <juanitobaldoaspler@gmail.com>
2025-08-08 06:00:16 +0800

    chatbot ui added

--- src/pages/public/index.tsx
+++ src/pages/public/index.tsx
@@ -1,7 +1,10 @@
+import { Chatbot } from "@/widget/chatbot";
+
 export const IndexPage = () => {
   return (
-    <div>
+    <main>
       <h1>Landing Page</h1>
-    </div>
+      <Chatbot />
+    </main>
   );
 };
