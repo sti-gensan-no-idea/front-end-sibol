b0a51ede2987fc738a21b2fa377e67c0335f9e2b
variantxx <juanitobaldoaspler@gmail.com>
2025-08-08 14:20:25 +0800

    developer signup added

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,7 @@ import { SignInPage } from "@/pages/public/signin";
 import { SignUpPage } from "@/pages/public/signup";
 import { AgentSignUpPage } from "@/pages/public/agent-signup";
 import { BrokerSignUpPage } from "@/pages/public/broker-signup";
+import { DeveloperSignUpPage } from "@/pages/public/developer-signup";
 import { DashboardClientPage } from "@/pages/client/dashboard-client";
 import { DashboardDeveloperPage } from "@/pages/developer/dashboard-developer";
 import { DashboardAgentPage } from "@/pages/agent/dashboard-agent";
@@ -19,9 +20,10 @@ function App() {
       <Route element={<SignInPage />} path="/signin" />
       <Route element={<SignUpPage />} path="/signup" />
 
-      {/* Sign Up of the 4 roles. */}
+      {/* Sign Up of the 3 roles. */}
       <Route element={<AgentSignUpPage />} path="/signup/agent" />
       <Route element={<BrokerSignUpPage />} path="/signup/broker" />
+      <Route element={<DeveloperSignUpPage />} path="/signup/developer" />
 
       <Route element={<DashboardClientPage />} path="/profile/client" />
       <Route element={<DashboardDeveloperPage />} path="/profile/developer" />
---
+++ src/pages/public/developer-signup.tsx
@@ -0,0 +1,9 @@
+import { DeveloperSignUpCardForm } from "@/widget/developer-signup-form";
+
+export const DeveloperSignUpPage = () => {
+  return (
+    <main>
+      <DeveloperSignUpCardForm />
+    </main>
+  );
+};
